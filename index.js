8e9466dd379f1682660f4708eb62f44e5c9a1b2d
Timothy Baskin <timfxofficial@gmail.com>
2021-03-26 00:19:56 -0500

    Finished Tasks

--- index.js
+++ index.js
@@ -42,8 +42,32 @@ class Airplane {
   */
   
  class Person {
-    
-  }
+    constructor(name, age){
+      this.name = name;
+      this.age = age;
+      this.stomach = [];
+    }
+
+    eat(someFood){
+      if (this.stomach.length < 10){
+        this.stomach.push(someFood);
+      }
+    }
+
+    poop(){
+      while(this.stomach.length > 0){
+        this.stomach.pop();
+      }
+    }
+
+    toString(){
+      return `${this.name}, ${this.age}`;
+    }
+
+
+    }
+
+  
   
   /*
     TASK 2
@@ -60,8 +84,32 @@ class Airplane {
   */
   
  class Car {
+   constructor(model, milesPerGallon){
+     this.model = model;
+     this.milesPerGallon = milesPerGallon;
+     this.tank = 0;
+     this.odometer = 0;
+   }
+
+   fill(gallons){
+     this.tank += gallons;
+   }
+
+  drive(distance){
+     this.odometer =distance;
+    // if (this.odometer>this.milesPerGallon){
+    //   this.tank++
+    // }
+    this.tank = Math.round((this.odometer/this.milesPerGallon));
+
+    if (this.tank === 0){
+      return `I ran out of fuel at ${this.odometer} miles!`
+    }
+
     
   }
+}
+  
   
   /*
     TASK 3
@@ -76,7 +124,15 @@ class Airplane {
           + {name} and {location} of course come from the instance's own properties.
   */
  class Lambdasian {
-    
+    constructor(attributes){
+      this.name = attributes.name;
+      this.age = attributes.age;
+      this.location = attributes.location;
+    }
+
+    speak(){
+      return `Hello my name is ${this.name}, I am from ${this.location}`
+    }
   }
   
   /*
@@ -93,7 +149,21 @@ class Airplane {
           + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
           + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
   */
- class Instructor {
+ class Instructor extends Lambdasian{
+   constructor(attributes){
+    super(attributes)
+     this.specialty = attributes.specialty;
+     this.favLanguage = attributes.favLanguage;
+     this.catchPhrase = attributes.catchPhrase;
+     }
+
+     demo(subject){
+      return `Today we are learning about ${subject}`;
+   }
+
+   grade(attributes, subject){
+     return `${attributes.name} receives a perfect score on ${subject}`;
+   }
 
  }
   /*
@@ -111,8 +181,25 @@ class Airplane {
           + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
           + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
   */
- class Student {
-     
+ class Student extends Lambdasian{
+     constructor(attributes){
+      super(attributes)
+       this.previousBackground = attributes.previousBackground;
+       this.className = attributes.className;
+       this.favSubjects = attributes.favSubjects;
+    }
+
+    listSubjects(){
+         return this.favSubjects.toString() + '!';
+    }
+
+    PRAssignment(subject){
+      return `${this.name} has submitted a PR for ${subject}`;
+    }
+
+    sprintChallenge(subject){
+      return `${this.name} has begun sprint challenge on ${subject}`
+    }
  }
   
   /*
@@ -128,7 +215,20 @@ class Airplane {
           + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
           + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
   */
- class ProjectManager {
+ class ProjectManager extends Instructor{
+   constructor(attributes){
+     super(attributes);
+     this.gradClassName = attributes.gradClassName;
+     this.favInstructor = attributes.favInstructor;
+   }
+
+   standUp(channel){
+      return `${this.name} announces to ${channel}, @channel standy times!`;
+   }
+
+   debugsCode(attributes, subject){
+     return `${attributes.name} debugs ${this.name}'s code on ${subject}`
+   }
      
  }
   /*
